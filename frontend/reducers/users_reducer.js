9363be2bf18a2d77dc54737ffc69310ba71c6798
Shawn Salat <shawn.salat@gmail.com>
2018-06-06 08:56:39 -0700

    add users reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,17 +1,13 @@
+import { merge } from 'lodash';
+
 import { RECEIVE_CURRENT_USER, 
   LOGOUT_CURRENT_USER } from '../actions/session_actions';
-
-const _nullSession = {
-  id: null
-};
 
 const sessionReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return { id: action.currentUser.id };
-    case LOGOUT_CURRENT_USER:
-      return _nullSession;
+      return merge({}, state, { [action.user.id]: action.user });
     default:
       return state;
   }
