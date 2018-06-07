4cca36b5e93775b0fd7ae59065e0da58f9298ccc
Shawn Salat <shawn.salat@gmail.com>
2018-06-07 10:44:47 -0700

    fix typo in session_form_page

--- frontend/components/session/sign_up_form.jsx
+++ frontend/components/session/sign_up_form.jsx
@@ -22,7 +22,7 @@ class SignUpForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const user = Object.assign({}, this.state);
-    this.props.processForm(user).then( res => this.props.history.push('/dashboards'));
+    this.props.processForm(user).then( res => this.props.history.push('/dashboard'));
   }
   renderErrors() {
     return(
