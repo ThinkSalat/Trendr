eb968b6920880cd9b07a1feeacdc270ae9ac1acf
Shawn Salat <shawn.salat@gmail.com>
2018-06-08 19:54:56 -0700

    create rouete for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/create'
-  get 'posts/update'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :show]
     resources :users, only: [:create, :update]
-    resources :posts
+    resources :posts, only: [:create, :update, :show, :index, :destroy]
     resources :likes, only: [:create, :destroy, :index]
     resources :follows, only: [:create, :destroy, :index]
     resource :search, only: [:show, :index]
