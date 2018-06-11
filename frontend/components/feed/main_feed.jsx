d681fa4d6aca15a4a488bb7bc6f4f0425136746c
Shawn Salat <shawn.salat@gmail.com>
2018-06-10 18:59:13 -0700

    css

--- frontend/components/feed/main_feed.jsx
+++ frontend/components/feed/main_feed.jsx
@@ -11,19 +11,19 @@ export default class MainFeed extends React.Component {
     return (
       <div className='main-feed-container'>
         <div className='main-content'>
-          <div className='main-content-feed col-1'>
-            <div> user avatar</div>
-            <img className='test' src="/images/post_type_div.png" alt=""/>
-              <NewPostNavContainer />
-              <Switch>
-                <ProtectedRoute exact path='/new/text' component={(props) => <FormContainer {...props} postType={'text'} />}  />
-                <ProtectedRoute exact path='/new/photo' component={(props) => <FormContainer {...props} postType={'photo'} />}  />
-                <ProtectedRoute exact path='/new/quote' component={(props) => <FormContainer {...props} postType={'quote'} />}  />
-                <ProtectedRoute exact path='/new/link' component={(props) => <FormContainer {...props} postType={'link'} />}  />
-                <ProtectedRoute exact path='/new/chat' component={(props) => <FormContainer {...props} postType={'chat'} />}  />
-                <ProtectedRoute exact path='/new/audio' component={(props) => <FormContainer {...props} postType={'audio'} />}  />
-                <ProtectedRoute exact path='/new/video' component={(props) => <FormContainer {...props} postType={'video'} />}  />
-              </Switch>
+          <ol className='main-content-feed col-1'>
+            <li><NewPostNavContainer />
+            <Switch>
+            <ProtectedRoute exact path='/new/text' component={(props) => <FormContainer {...props} postType={'text'} />}  />
+            <ProtectedRoute exact path='/new/photo' component={(props) => <FormContainer {...props} postType={'photo'} />}  />
+            <ProtectedRoute exact path='/new/quote' component={(props) => <FormContainer {...props} postType={'quote'} />}  />
+            <ProtectedRoute exact path='/new/link' component={(props) => <FormContainer {...props} postType={'link'} />}  />
+            <ProtectedRoute exact path='/new/chat' component={(props) => <FormContainer {...props} postType={'chat'} />}  />
+            <ProtectedRoute exact path='/new/audio' component={(props) => <FormContainer {...props} postType={'audio'} />}  />
+            <ProtectedRoute exact path='/new/video' component={(props) => <FormContainer {...props} postType={'video'} />}  />
+            </Switch></li>
+
+            <div className='main-content-feed-avatar'> user avatar</div>
             <div className='main-feed'>  {/*  <MainFeedContainer /> */}
               <div>avatar</div>
               <div>post</div>
@@ -31,7 +31,7 @@ export default class MainFeed extends React.Component {
             <ul>
               <li>post items</li>
             </ul>
-          </div>
+          </ol>
         </div>
         <div className='main-content-sidebar col-2'>
           <div>sidebar</div>
