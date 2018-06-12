97729f9882d5c8ffad74dc6ff1cf075b284747a1
Shawn Salat <shawn.salat@gmail.com>
2018-06-11 21:06:21 -0700

    remove planning comments from component, get ready for heroku push

--- frontend/components/posts/post.jsx
+++ frontend/components/posts/post.jsx
@@ -52,9 +52,7 @@ export default class Post extends React.Component {
             </ul>
           </li>
           <li className='post-title' >{post.title}</li>
-          {/* <li className='post-body' >{post.body || post.summary}</li> */}
           <li className='post-body' dangerouslySetInnerHTML={{__html: (post.body || post.summary)}}></li>
-          {/* Post-nav can be replaced by component  */}
           <li><PostBottomNav post={post}/></li>
         </ul>
       </div>
