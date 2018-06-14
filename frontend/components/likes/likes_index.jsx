2ac6eec2353175233acfa3b2f37e633594b5cb2d
Shawn Salat <shawn.salat@gmail.com>
2018-06-14 09:58:33 -0700

    >likes index 404s but leaves url

--- frontend/components/likes/likes_index.jsx
+++ frontend/components/likes/likes_index.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+
 
 import FeedPostContainer from '../posts/feed_post_container';
 
 export default class LikesIndex extends React.Component {
   constructor(props) {
     super(props);
-    //can check if current path is users/:id or /likes (defaults to current user)
 
+    this.state = {
+      isAvailable: true
+    };
   }
 
   componentDidMount() {
-    this.props.getLikedPosts(this.props.userId);
+    this.props.getLikedPosts(this.props.userId)
+      .then( null, err => this.setState({isAvailable: false}));
   }
 
   componentWillReceiveProps(newProps) {
     if (newProps.userId !== this.props.userId) {
-      this.props.getLikedPosts(this.props.userId);
+      this.props.getLikedPosts(this.props.userId)
+        .then( null, err => this.setState({isAvailable: false}));
     }
   }
   
   render() {
-    // if (!this.state.isAvailable) {
-    //   return(
-    //     <Redirect to="/404" />
-    //   );
-    // }
+    if (!this.state.isAvailable) {
+      return(
+        <Redirect to={`/404/users/${this.props.match.params.userId}/likes`}/>
+      );
+    }
 
     //if no liked posts, return a diff page that says they have no liked posts and a link to the explore page
 
