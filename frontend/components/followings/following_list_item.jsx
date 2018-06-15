46a607668f59208af27aa2bfa200fdda631d86ef
Shawn Salat <shawn.salat@gmail.com>
2018-06-14 22:20:17 -0700

    added master styling to follow pages

--- frontend/components/followings/following_list_item.jsx
+++ frontend/components/followings/following_list_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import FollowingButton from '../users/following_button_container';
 
@@ -7,14 +8,23 @@ export default class FollowingListItem extends React.Component {
     super(props);
   }
 
+  followsYou() {
+    return this.props.user ? 'follows-you' : 'not-follows-you';
+  }
+
   render() {
     const { user } = this.props;
     return(
       <li className='following-list-item'>
-        <ul className='following-list-item-details'>
-          <li className='following-list-title'> {user.title} </li>
-          <li className='following-list-username'> {user.username} </li>
-          <li className='following-switcher-container'><FollowingButton currentUser={this.props.currentUser} userId={user.id} /></li>
+          <ul className='following-list-item-details'>
+            <Link className='link-wrapper' to={`/users/${user.id}`}>
+              <div className='following-avatar'> <img src={user.avatar}></img> </div>
+              <li className='following-list-title'> {user.title} </li>
+              <li className='following-list-username'> {user.username} 
+                {/* <span className={`${this.followsYou()} following-list-follows-you`}>Follows You</span> */}
+              </li>
+            </Link>
+            <li className='following-switcher-container'><FollowingButton currentUser={this.props.currentUser} userId={user.id} /></li>
         </ul>
       </li>
     );
