33c1433167daab079915b4563e56d500b6743c2b
Shawn Salat <shawn.salat@gmail.com>
2018-07-18 14:44:32 -0700

    fix following

--- frontend/components/users/following_button.jsx
+++ frontend/components/users/following_button.jsx
@@ -9,32 +9,23 @@ export default class FollowingButton extends React.Component {
   following() {
     return this.props.currentUser.followedUsers.includes(parseInt(this.props.userId));
   }
-  follow() {
+  
+  followInfo() {
     return {follow: {
       follower_id: this.props.currentUser.id,
       followed_id: this.props.userId
     }};
   }
 
-   changeFollowStatus() {
-     console.log(this.state.following, 'following or not');
+  changeFollowStatus() {
    if (this.state.following) {
      this.setState({following: false})
-      this.props.unfollowUser(this.follow());
+      this.props.unfollowUser(this.followInfo());
     } else { 
       this.setState({following: true})
-       this.props.followUser(this.follow());
+       this.props.followUser(this.followInfo());
     }
   }
-  // changeFollowStatus() {
-  //  if (this.following()) {
-  //    this.setState({following: false})
-  //    this.props.unfollowUser(this.follow());
-  //   } else { 
-  //     this.setState({following: true})
-  //     this.props.followUser(this.follow());
-  //   }
-  // }
 
   followDisplay() {
     // return this.props.currentUser.followedUsers.includes(parseInt(this.props.userId)) ?  'Unfollow' : 'Follow'; 
@@ -42,53 +33,7 @@ export default class FollowingButton extends React.Component {
   }
   render() {
     return(
-      <div className='following-switcher' onClick={() => this.changeFollowStatus.bind(this)}>{this.followDisplay()}</div>
+      <div className='following-switcher' onClick={this.changeFollowStatus.bind(this)}>{this.followDisplay()}</div>
     );
    }
 }
-
-// refactored code above worked and then stopped working. 
-
-// import React from 'react';
-
-// export default class FollowingButton extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       following: props.currentUser.followedUsers.includes(parseInt(props.userId)),
-//       follow: {
-//         follower_id: props.currentUser.id,
-//         followed_id: props.userId
-//       }
-//     };
-//   }
-
-//   // ..current user not updating
-
-//   componentWillReceiveProps(newProps) {
-//     if (newProps.userId !== this.props.userId) {
-//       this.setState({
-//         following: newProps.currentUser.followedUsers.includes(parseInt(newProps.userId)),
-//         follow: { 
-//           followed_id: newProps.userId,
-//           follower_id: this.props.currentUser.id
-//         }
-//       });
-//     }
-//   }
-   
-//   changeFollowStatus() {
-//     (this.props.currentUser.followedUsers.includes(parseInt(this.props.userId)) ? this.props.unfollowUser(this.state) : this.props.followUser(this.state)).then(() => {
-//       this.setState({following: !this.state.following});
-//     });
-//   }
-
-//   followDisplay() {
-//     return this.props.currentUser.followedUsers.includes(parseInt(this.props.userId)) ?  'Unfollow' : 'Follow'; 
-//   }
-//   render() {
-//     return(
-//       <div className='following-switcher' onClick={() => this.changeFollowStatus()}>{this.followDisplay()}</div>
-//     );
-//    }
-// }
