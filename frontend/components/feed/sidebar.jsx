ea6dfb39ca3b4ff4c87cf32d52e35008d4cbaa83
Shawn Salat <shawn.salat@gmail.com>
2018-08-09 14:42:30 -0700

    remove items from ux that aren't implemented

--- frontend/components/feed/sidebar.jsx
+++ frontend/components/feed/sidebar.jsx
@@ -14,7 +14,7 @@ export default class Sidebar extends React.Component {
           <li><Link to='/likes'><span className='sidebar-icon' >&#59983;</span><span className='sidebar-text' >Likes</span></Link></li>
           <li><Link to='/followed_users'><span className='sidebar-icon' >&#59992;</span><span className='sidebar-text' >Followed Blogs</span></Link></li>
           <li><Link to='/followers'><span className='sidebar-icon' >&#59972;</span><span className='sidebar-text' >Followers</span></Link></li>
-          <li><Link className='disabled-link' onClick={(e) => e.preventDefault()}  to='/'><span className='sidebar-icon' >&#60089;</span><span className='sidebar-text' >Collections</span></Link></li>
+          {/* <li><Link className='disabled-link' onClick={(e) => e.preventDefault()}  to='/'><span className='sidebar-icon' >&#60089;</span><span className='sidebar-text' >Collections</span></Link></li> */}
         </ul>
       </div>
     );
