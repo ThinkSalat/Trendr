a142f9181ecb84eef4859ca34a945f1d1b608abe
Shawn Salat <shawn.salat@gmail.com>
2018-08-28 18:04:11 -0700

    add search actions and api call

---
+++ frontend/actions/search_actions.js
@@ -0,0 +1,12 @@
+import * as SearchAPIUtil from '../util/search_api_util';
+
+export const SEARCH = 'SEARCH';
+
+export const search = () => dispatch => (
+  SearchAPIUtil.search().then(res => dispatch(searchActionCreator(res)))
+);
+
+const searchActionCreator = ({userSearchResults}) => ({
+  type: SEARCH,
+  userSearchResults
+})
