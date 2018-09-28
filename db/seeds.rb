0b9064bcc527c04a2dec5ef58b461d6312c3129b
Shawn Salat <shawn.salat@gmail.com>
2018-09-28 13:57:05 -0700

    seeding new posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -100,3 +100,80 @@ require 'open-uri'
 #               'https://i.imgur.com/OpW7qCd.png']
 #   user.avatar.attach(io: open(avatars.sample), filename: 'default')
 # end
+
+
+def random_date
+  year = rand(11) + 2008
+  month = rand(12) + 1
+  day = rand(30) + 1
+  hour = rand(24)
+  minute = rand(60)
+  second = rand(60)
+  DateTime.new(year, month, day, hour, minute, second)
+end
+
+def createPosts(posts, user_id)
+  posts.each do |seed_post|
+        next unless ['photo','text','photoset'].include?(seed_post['type'])
+    
+        post = {}
+        post['user_id'] = user_id
+        post['post_type'] = seed_post['type']
+        post['body'] = seed_post['body']
+        post['summary'] = seed_post['caption']
+        post['title'] = seed_post['title']
+        post['source_url'] = seed_post['source_url']
+        post['source_title'] = seed_post['source_title']
+        post['slug'] = seed_post['slug']
+        post['caption'] = seed_post['caption']
+        post['post_type'] = seed_post['type']
+        post['state'] = 'published'
+
+        date = random_date
+        post["created_at"] = date
+        post["updated_at"] = date
+
+        Post.create!(post)
+        
+        puts '-------Post Created!----------'
+    
+        puts '-----Uploading Post Media-----'
+        post = Post.last
+        seed_post['photos'].each do |photo|
+          print 'downloading '
+          puts photo['original_size']['url']
+          media = open(photo['original_size']['url'])
+          i = 0
+          post.images.attach(
+            io: media,
+            filename: "post_id" + post['id'].to_s + 'photo' + i.to_s
+          )
+          i += 1
+        end unless seed_post['photos'].nil?
+      end
+end
+
+user_id = User.find_by_username("darksilenceinsuburbia")[:id]
+Dir.foreach('./db/blogs') do |file|
+    next if file[0] == '.'
+    next unless file == 'darksilence_more.json'
+    puts '==================================='
+    puts file
+    puts '==================================='
+    file = File.read("./db/blogs/#{file}")
+    file = JSON.parse(file)
+    posts = file["response"]["posts"]
+    createPosts(posts, user_id)
+end
+
+
+
+def random_date
+  year = rand(11) + 2008
+  month = rand(12) + 1
+  day = rand(30) + 1
+  hour = rand(24)
+  minute = rand(60)
+  second = rand(60)
+  DateTime.new(year, month, day, hour, minute, second)
+end
